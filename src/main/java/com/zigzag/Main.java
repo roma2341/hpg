bcda72c2b6f1c250f33d9441f1e0acf3e2c40d16
Roman zinchuk <zigzag2341@gmail.com>
2018-06-13 17:50:11 +0300

    Added pom dependencies and example and maven compiler module

---
+++ src/main/java/com/zigzag/Main.java
@@ -0,0 +1,9 @@
+package com.zigzag;
+
+import com.zigzag.example.HFJavaSDKBasicExample;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HFJavaSDKBasicExample.main(args);
+    }
+}
